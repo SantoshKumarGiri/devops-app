e08a386a298b1b91ffa88e2da26053280506e98f
Santosh Kumar Giri <santoshraj.nic@gmail.com>
2026-03-29 03:09:52 +0530

    updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('DevOps Project is Live 🚀 and created by Santosh Kumar Giri:');
+  res.send('DevOps Project is Live 🚀');
 });
 
 app.listen(3000, () => {
